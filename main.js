0ecc3abb5d1a8ef88838c44f993b3a487af0d8b8
joshuaneronha <77899869+joshuaneronha@users.noreply.github.com>
2021-03-19 16:10:18 -0400

    working map

--- main.js
+++ main.js
@@ -17,28 +17,36 @@ let svg_2 = d3.select("#graph2")
 
 var map_q2 = d3.map()
 
-
-var map_proj = d3.geoMercator().translate([graph_1_width / 2, graph_1_height / 2]);
 var outlines = d3.geoPath()
+var map_proj = d3.geoMercator().scale(50).center([0,0]).translate([graph_1_width / 2, graph_1_height / 2]);
+
 
 var color = d3.scaleThreshold()
-    .domain(d3.range(2, 10))
-    .range(d3.schemeBlues[9]);
+  .domain([0, 10, 20, 30, 40, 50, 60, 70, 80, 90,100])
+  .range(d3.schemeBlues[9]);
 
 var dataset = [
-    d3.json("https://unpkg.com/world-atlas@1/world/110m.json"),
-    d3.csv("/data/football_clean_q2.csv", function (d) {map_q2.set(d.country_code, d.win_percent)})
+    d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"),
+    d3.csv("/data/football_clean_q2.csv", function (d) {map_q2.set(d.country_code, +parseFloat(d.win_percent))})
 ]
+
+console.log(map_q2)
 
 Promise.all(dataset).then(execute)
 
 function execute([input]) {
-    svg_2.append("g").attr("class", "countries")
+    svg_2.append("g")
+    .attr("class","countries")
     .selectAll("path")
-    .data(topojson.feature(input, input.objects.countries).features)
+    .data(input.features)
     .enter().append("path")
-    .attr("fill", function(d) { return color(d.win_percent = map_q2.get(d.country_code)); })
-    .attr("d", outlines)
+    .attr("d", d3.geoPath().projection(map_proj))
+    // .attr("fill", function(d) { return color(d.win_percent = map_q2.get(d.country_code)); })
+    .attr("fill", function (d) {
+        d.total = map_q2.get(d.id);
+        console.log(d.total)
+        return color(d.total);
+      });
 
 
 }
