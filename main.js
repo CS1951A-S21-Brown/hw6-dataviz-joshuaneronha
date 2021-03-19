2318957e5ced64d91b5e0809f0744e89a242b363
joshuaneronha <77899869+joshuaneronha@users.noreply.github.com>
2021-03-19 17:35:41 -0400

    map

--- main.js
+++ main.js
@@ -21,14 +21,16 @@ var outlines = d3.geoPath()
 var map_proj = d3.geoMercator().scale(50).center([0,0]).translate([graph_1_width / 2, graph_1_height / 2]);
 
 
-var color = d3.scaleThreshold()
-  .domain([0, 10, 20, 30, 40, 50, 60, 70, 80, 90,100])
-  .range(d3.schemeBlues[9]);
+// var color = d3.scaleThreshold()
+//   .domain([0, 10, 20, 30, 40, 50, 60, 70, 80, 90,100])
+//   .range(d3.schemeBlues[9]);
 
 var dataset = [
     d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"),
     d3.csv("/data/football_clean_q2.csv", function (d) {map_q2.set(d.country_code, +parseFloat(d.win_percent))})
 ]
+
+var color = d3.scaleSequential(d3.interpolateYlGnBu).domain([0, 70])
 
 console.log(map_q2)
 
